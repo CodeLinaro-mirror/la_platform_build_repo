b5f62570c7e6fa23a4298407de76072f820a27a3
Bharat Pawar <bpawar@codeaurora.org>
2021-07-19 10:08:43 +0530

    Enable SKIP_ABI_CHECKS config for build compilation
    Change-Id: I8efa10e6ceb3b4d12f5208641cc3fc68692b9cb8

--- envsetup.sh
+++ envsetup.sh
@@ -324,6 +324,9 @@ function setpaths()
     unset ANDROID_TARGET_OUT_TESTCASES
     export ANDROID_TARGET_OUT_TESTCASES=$(get_abs_build_var TARGET_OUT_TESTCASES)
 
+    # TODO: fix and remove below check
+    export SKIP_ABI_CHECKS=true
+
     # needed for building linux on MacOS
     # TODO: fix the path
     #export HOST_EXTRACFLAGS="-I "$T/system/kernel_headers/host_include
