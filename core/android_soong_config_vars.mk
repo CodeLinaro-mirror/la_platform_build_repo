1961ce54043352a4c71414cad52f0fbf3754122e
Sunish Nambayil <quic_snambayi@quicinc.com>
2023-05-08 15:50:00 +0530

    ES: Add emulator flag
    build time soong var for qemu.
    
    Change-Id: Ie903e523fc7a65b79583a8ae89669610176e6e8f
    Signed-off-by: Sunish Nambayil <quic_snambayi@quicinc.com>

--- core/android_soong_config_vars.mk
+++ core/android_soong_config_vars.mk
@@ -36,6 +36,7 @@ $(call add_soong_config_var,ANDROID,BOARD_USES_RECOVERY_AS_BOOT)
 $(call add_soong_config_var,ANDROID,BOARD_BUILD_SYSTEM_ROOT_IMAGE)
 $(call add_soong_config_var,ANDROID,BOARD_SUPPORTS_RAMDISK_EARLY_INIT)
 $(call add_soong_config_var,ANDROID,PRODUCT_INSTALL_DEBUG_POLICY_TO_SYSTEM_EXT)
+$(call add_soong_config_var,ANDROID,BUILD_QEMU_IMAGES)
 
 # Default behavior for the tree wrt building modules or using prebuilts. This
 # can always be overridden by setting the environment variable
