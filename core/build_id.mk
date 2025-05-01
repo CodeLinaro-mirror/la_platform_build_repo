08b5031bb88e3dafe4372ec8f05b7377f3e1f779
Steve Kim <stevenxkim@google.com>
2025-05-01 01:02:57 +0000

    Version bump to ZP1A.250428.001
    Change-Id: I3055a3423e785bbbc92d93f5e6a5eddb024cf46d

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,8 +18,8 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=ZP1A.250419.001
+BUILD_ID=ZP1A.250428.001
 
 
 
-BUILD_ID:=${BUILD_ID}-ZP1A.250419.001
+BUILD_ID:=${BUILD_ID}-ZP1A.250428.001
