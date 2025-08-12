b7cd7505caf714b973716c15f86a95644685e3c8
Steve Kim <stevenxkim@google.com>
2025-08-12 05:30:11 +0000

    Version bump to BP4A.250811.001
    Change-Id: Icc8911f7b1afedc3cf4f9ccbf96a08f5b84aa88a

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,8 +18,8 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=BP4A.250806.001
+BUILD_ID=BP4A.250811.001
 
 
 
-BUILD_ID:=${BUILD_ID}-BP4A.250806.001
+BUILD_ID:=${BUILD_ID}-BP4A.250811.001
