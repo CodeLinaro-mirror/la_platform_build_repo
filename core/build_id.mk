211e1b964e107253fcc655191e9e8bcad550b29d
Deyao Ren <deyaoren@google.com>
2025-09-13 19:02:10 +0000

    Version bump to BP4A.250912.001
    Change-Id: Ic4967427c1d5880f3ccf28a38e6d241169580d7b

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,8 +18,8 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=BP4A.250908.001
+BUILD_ID=BP4A.250912.001
 
 
 
-BUILD_ID:=${BUILD_ID}-BP4A.250908.001
+BUILD_ID:=${BUILD_ID}-BP4A.250912.001
