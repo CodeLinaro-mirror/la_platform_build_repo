066ea096d57637deb14c394fccc64d2190b2b4a8
Eric Taseski <etaseski@google.com>
2025-12-22 10:44:54 -0800

    Version bump to ZP1A.251217.001
    Change-Id: I6515931a4331e146818b6e7ab844f4725bc6f5d1

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,8 +18,8 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=CP2A.251215.001
+BUILD_ID=ZP1A.251217.001
 
 
 
-BUILD_ID:=${BUILD_ID}-CP2A.251215.001
+BUILD_ID:=${BUILD_ID}-ZP1A.251217.001
