0b9a6cdbd44256e951cce8a08988743000efd621
Deyao Ren <deyaoren@google.com>
2026-01-13 19:15:22 +0000

    Version bump to CP2A.260109.001
    Change-Id: I5b8c64ee521a1db1d02a0382194d14e21b0262c1

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,8 +18,8 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=CP2A.260107.001
+BUILD_ID=CP2A.260109.001
 
 
 
-BUILD_ID:=${BUILD_ID}-CP2A.260107.001
+BUILD_ID:=${BUILD_ID}-CP2A.260109.001
