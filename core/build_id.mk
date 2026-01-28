935398aa24776b83d78e0668caf0edb3fa009116
Keystone Automerger <keystone-automerger.google.com@appspot.gserviceaccount.com>
2026-01-28 21:11:02 +0000

    Version bump to CP2A.260128.001
    Change-Id: Ib480c3e67047c3f2fd249596069eef94dde7b6db

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,8 +18,8 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=CP2A.260125.001
+BUILD_ID=CP2A.260128.001
 
 
 
-BUILD_ID:=${BUILD_ID}-CP2A.260125.001
+BUILD_ID:=${BUILD_ID}-CP2A.260128.001
